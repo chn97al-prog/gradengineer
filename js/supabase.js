9671b519988e93913635418d6d8c86325f42218d
chn97al-prog <chn97al@gmail.com>
2026-07-11 22:30:32 +0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+/* ==========================================
+   تجهيزات المهندس
+   Supabase
+========================================== */
+
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const supabaseUrl = "https://wwibtjohymbwtfhcylxy.supabase.co";
+
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3aWJ0am9oeW1id3RmaGN5bHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3ODk1ODEsImV4cCI6MjA5OTM2NTU4MX0.ZxohqQqBXorEfcN2bar3eyzTlBfTnAq08vPZMF1JtbQ";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
